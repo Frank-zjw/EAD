07be023d3e35715100982df88fce73e22869d617
Frank-zjw <44082664+Frank-zjw@users.noreply.github.com>
2018-10-12 15:16:17 +0800

    Update if.php

---
+++ if.php
@@ -0,0 +1,18 @@
+<?php
+
+$x= 9;
+
+switch($x) {
+    case "":  
+     echo "equaly";
+     break;
+    case "$x > $y":
+     echo "bigger";
+     break;
+    case "$x < $y":
+     echo "smaller";
+     break;
+    default:
+    echo "diffrerent type";    
+}
+?>
